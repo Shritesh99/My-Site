cbe0610b80b6715cf84ed72d573cbeb12a67f3e3
Shritesh <shritesh.sj@gmail.com>
2019-05-07 21:30:17 +0530

    Initialize bulma

---
+++ src/Components/Landing.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Landing extends Component{
+     render() {
+         return(
+           <div className="hero section is-success"></div>
+         );
+     }
+}
+
+export default Landing;
